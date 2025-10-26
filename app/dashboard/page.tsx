cceb19e5301d36f2d76ad8c259edff38cef69180
NicaSystems <nicasystems.nic@gmail.com>
2025-10-26 17:37:26 -0600

    avance del capitulo 8

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,47 @@
-export default function DashboardPage() {
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+ 
+export default async function Page() {
+  const url = "http://localhost:5000"
+  const data = await fetch(`${url}/fetch-revenue`)
+  const revenue = await data.json()
+  
+  const data2 = await fetch(`${url}/fetch-last-invoices`)
+  const latestInvoices = await data2.json()
+  console.log("revenue", revenue)
+  console.log("latestInvoices", latestInvoices)
+
+
+  const data3 = await fetch(`${url}/fetch-card-data`)
+  const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await data3.json()
+
+
+  console.log("numberOfCustomers", numberOfCustomers)
+  console.log("numberOfInvoices", numberOfInvoices)
+  console.log("totalPaidInvoices", totalPaidInvoices)
+  console.log("totalPendingInvoices", totalPendingInvoices)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
-      <p className="text-lg text-gray-700">Welcome to your dashboard!</p>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card 
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
     </main>
   );
 }
--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,6 +1,8 @@
-import { Raleway, Lato, Lusitana } from "next/font/google";
+import { Raleway, Lato, Lusitana, Inter } from "next/font/google";
 
 export const raleway = Raleway({subsets: ["latin"]});
 export const lato = Lato({subsets: ["latin"], weight: ["400", "700"]});
 
 export const lusitana = Lusitana({subsets: ["latin"], weight: ["400", "700"]});
+
+export const inter = Inter({subsets: ["latin"], weight: ["400", "600", "700"]});
